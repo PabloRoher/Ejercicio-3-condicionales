a0dd6188e71b2b04b5cbbb347c8f875815718f9b
Pablo <113892146+PabloRoher@users.noreply.github.com>
2023-02-17 01:04:07 +0000

    Update ElTiempo.java

--- Ejercicio 3 condicionales/src/main/java/ElTiempo.java
+++ Ejercicio 3 condicionales/src/main/java/ElTiempo.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class ElTiempo {
     public static void main(String[] args){
-        double c;
+        int c;
         System.out.println("Introduce la temperatura actual en grados: ");
         Scanner teclado1 = new Scanner(System.in);
-        c = teclado1.nextDouble();
+        c = teclado1.nextint();
 
         if (c < 2.0){
             System.out.println("Riesgo de hielo");
-        }else {
-            System.out.println("No hay riesgo de hielo");
+        }else if (2<= c && c < 15) {
+            System.out.println("Hace frio");
+        } else if (15< c && c < 30) {
+            System.out.println("Buena temperatura");
+        } else if (c > 30) {
+            System.out.println("Demasiado calor");
         }
     }
 }
